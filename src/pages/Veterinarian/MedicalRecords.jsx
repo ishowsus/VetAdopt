bfe0f5f5379424f78362f23ae2d5c9a55c6a6f89
Nash Aaron Lapincao <ishowsus@gmail.com>
2026-09-22 23:21:57 +0800

    Update MedicalRecords.jsx

--- src/pages/Veterinarian/MedicalRecords.jsx
+++ src/pages/Veterinarian/MedicalRecords.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { collection, getDocs } from "firebase/firestore";
-import { db } from "../../Firebase";
+import { db } from "../../firebase";
 
 function MedicalRecords() {
   const [records, setRecords] = useState([]);
